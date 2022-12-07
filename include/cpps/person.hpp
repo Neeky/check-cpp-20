75e58102e333038d0442759dcce8fc89d70284cd
蒋乐兴 <1721900707@qq.com>
2022-12-07 15:46:21 +0800

    feat: 添加宏定义

--- include/cpps/person.hpp
+++ include/cpps/person.hpp
@@ -1,3 +1,6 @@
+#ifndef CPPS_PERSON__
+#define CPPS_PERSON__
+
 #include <iostream>
 #include <string>
 
@@ -6,6 +9,7 @@ class Person
 public:
     Person(std::string name, std::size_t age);
     std::string sayHello() const;
+    std::string get_name() const;
     bool operator<(const Person &other);
     bool operator<(double age);
     friend std::ostream& operator<<(std::ostream& stream, Person& person);
@@ -16,3 +20,5 @@ private:
     size_t age;
     std::string name;
 };
+
+#endif // CPPS_PERSON__
