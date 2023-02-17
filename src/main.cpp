df48f4ccecb3857bc7b92524cc524f06fcc81c6b
Neeky <1721900707@qq.com>
2023-02-17 20:48:42 +0800

    feat: 函数耗时追踪

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
-#include "cpps/person.hpp"
+#include <unistd.h>
+#include <iostream>
+//#include <stdio.h>
+
+
 using namespace std;
+
+
+int hello(int i) 
+{
+    cout<<"hello world " << i << " ." << endl;
+    // sleep(0);
+    return i;
+}
 
 int main(int argc, char *argv[])
 {
-    Person tom = Person("cpps", 18);
-    cout<<tom<<endl;
+    for(int i = {0}; i < 3; i++) 
+    {
+        hello(i);
+    }
     return 0;
 }
